096e3b83ea68007487f0b640520309fef49458f7
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-04-04 09:57:39 -0500

    Implemented removeHead method to LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -18,6 +18,24 @@ class LinkedList {
         this.size++;
     }
 
+    removeHead() {
+        const removedHead = this.head;
+
+        if (!removedHead) {
+            return null;
+        }
+
+        if (!removedHead.getNextNode()) {
+            this.head = null;
+        } else {
+            this.head = removedHead.getNextNode();
+            removedHead.setNextNode(null);
+        }
+
+        this.size--;
+        return removedHead;
+    }
+
     printList() {
         let str = '<head> ';
         let current = this.head;
@@ -36,6 +54,10 @@ const list = new LinkedList();
 list.addToHead(1);
 list.addToHead(2);
 list.addToHead(3);
+list.removeHead();
+list.removeHead();
+list.removeHead();
+list.removeHead();
 
 console.log(list.size);
 console.log(list.printList());
