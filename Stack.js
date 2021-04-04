a7b97857878a8d1a725200bd9827b64a4a61f781
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-04-04 11:54:03 -0500

    Implemented push, pop, and peek methods in Stack. Basic Stack complete

--- Stack.js
+++ Stack.js
@@ -14,4 +14,33 @@ class Stack {
     isEmpty() {
         return this.size === 0;
     }
+
+    push(data) {
+        if (!this.hasRoom()) {
+            throw Error('Stack is full');
+        } else {
+            this.stack.addToHead(data);
+            this.size++;
+        }
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            throw Error('Stack is empty');
+        } else {
+            const removedHead = this.stack.removeHead();
+            this.size--;
+            return removedHead;
+        }
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            throw Error('Stack is empty');
+        } else {
+            return this.stack.head.data;
+        }
+    }
 }
+
+module.exports = Stack;
