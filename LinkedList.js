0b6b9ede61f6accb2e2979461c3572cb4c7763db
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-04-04 09:50:18 -0500

    Implemented printList method on LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -1,8 +1,25 @@
-const Node = require(',/Node.js');
+const Node = require('./Node.js');
 
 class LinkedList {
     constructor() {
         this.head = null;
         this.size = 0;
     }
+
+    printList() {
+        let str = '<head> ';
+        let current = this.head;
+
+        while (current) {
+            str += `${current.data} `;
+            current = current.getNextNode();
+        }
+
+        str += '<tail>';
+        return str;
+    }
 }
+
+const list = new LinkedList();
+
+console.log(list.printList());
