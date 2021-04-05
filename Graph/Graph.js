ac0780c7b7bea4f8982093999c08da9546093eaa
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-04-05 13:13:20 -0500

    Implemented addVertex in Graph.js

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -7,4 +7,16 @@ class Graph {
         this.isWeighted = isWeighted;
         this.isDirected = isDirected;
     }
+
+    addVertex(data) {
+        const newVertex = new Vertex(data);
+        this.vertices.push(newVertex);
+        return newVertex;
+    }
 }
+
+const g = new Graph();
+
+const one = g.addVertex(1);
+
+console.log(g);
