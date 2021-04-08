d810c01e4932f9131ee64307ec2b1d4ae1d572bc
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-04-08 18:32:30 -0500

    Implemented exists, isEmpty, swap, and canSwap methods in PriorityQueue.js

--- Graph/PriorityQueue.js
+++ Graph/PriorityQueue.js
@@ -4,6 +4,23 @@ class PriorityQueue {
         this.size = 0;
     }
 
+    canSwap(current, left, right) {
+        return (this.exists(left) && this.heap[left].priority < this.heap[current].priority) 
+            || (this.exists(right) && this.heap[right].priority < this.heap[current].priority);
+    }
+
+    swap(a, b) {
+        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    } 
+
+    exists(index) {
+        return index <= this.size;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
     getParent(current) {
         return Math.floor(current / 2);
     }
